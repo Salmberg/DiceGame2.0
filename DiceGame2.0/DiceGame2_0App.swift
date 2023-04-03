455d6701751de2e21598f945dc90ad16edacc81f
Salmberg <60116309+Salmberg@users.noreply.github.com>
2023-04-03 14:53:08 +0200

    Initial Commit

---
+++ DiceGame2.0/DiceGame2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  DiceGame2_0App.swift
+//  DiceGame2.0
+//
+//  Created by David Salmberg on 2023-04-03.
+//
+
+import SwiftUI
+
+@main
+struct DiceGame2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
