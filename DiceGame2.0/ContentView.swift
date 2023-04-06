e27ee08ed0d11a3f83b4d2ce5d85b4c1fa0e3245
Salmberg <60116309+Salmberg@users.noreply.github.com>
2023-04-06 18:09:25 +0200

    Now i can pass bid value from BetSheet to the content view

--- DiceGame2.0/ContentView.swift
+++ DiceGame2.0/ContentView.swift
@@ -12,40 +12,58 @@ struct ContentView: View {
     @State var playerDice = 0
     @State var computerSum = 0
     @State var playerSum = 0
+    @State var newBet = 0
+    @State var bet = 0
     @State var showingBustSheet = false
-
+    @State var showingBetSheet = false
+    
 
     
     var body: some View {
+        
         
         ZStack {
             Color(red: 40/256, green: 108/256, blue: 59/256)
                 .ignoresSafeArea()
             
-            
             VStack {
-                
-                Text("Computers dice")
-                    .font(.largeTitle)
-                Text("\(computerSum)")
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .fontWeight(.bold)
-                Spacer()
-                
-                DiceView(theDice: computerDice)
-                
-                Text("\(playerSum)")
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .fontWeight(.bold)
-                DiceView(theDice: playerDice)
-                Spacer()
-                
-                
+                Text("Your current bet: \(newBet)$")
+                HStack {
+                    Spacer()
+                    Text("Computer")
+                        .font(.largeTitle)
+                        .padding(30)
+                   
+                    Spacer()
+                    Text("Player")
+                        .font(.largeTitle)
+                        .padding(30)
+                    
+                                    }
                 HStack {
                     
+                    Text("\(computerSum)")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                        .padding(70)
+                    
+                    Spacer()
+                    Text("\(playerSum)")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                        .padding(70)
+                  
 
+                }
+                HStack {
+                    DiceView(theDice: computerDice)
+                    
+                    DiceView(theDice: playerDice)
+                                    }
+        
+                HStack {
                     
                 }.onAppear() {
                     //newDiceValue()
@@ -54,7 +72,20 @@ struct ContentView: View {
                     rollTheDice()
                     
                 }, label: {
-                    Text("Roll your dice")
+                    Text("Roll the dice")
+                        .font(.largeTitle)
+                        .padding()
+                        .foregroundColor(Color.white)
+                })
+                .background(Color.red)
+                .cornerRadius(15.0)
+                Spacer()
+                
+                Button(action: {
+                    //go to place bet view
+                    showingBetSheet = true
+                }, label: {
+                    Text("Bet on the players")
                         .font(.largeTitle)
                         .padding()
                         .foregroundColor(Color.white)
@@ -70,17 +101,24 @@ struct ContentView: View {
         }, content: {
             BustSheet(computerSum: computerSum, playerSum: playerSum)
         })
+        
+        .sheet(isPresented: $showingBetSheet, onDismiss: {
+            computerSum = 0
+            playerSum = 0
+            
+            }, content: {
+                BetSheet(computerSum: computerSum, playerSum: playerSum, newBet: $newBet)
+        })
     }
     
     
     func rollTheDice(){
        newDiceValue()
-        
         computerSum += computerDice
         playerSum += playerDice
-        
-       
     }
+    
+   
     
     func newDiceValue(){
         computerDice = Int.random(in: 1...6)
@@ -125,7 +163,7 @@ struct BustSheet : View {
                     .foregroundColor(.black)
                     .font(.title)
                     .padding()
-                Text("and you got \(playerSum)")
+                Text("Player got \(playerSum)")
                     .foregroundColor(.black)
                     .font(.title)
                     .padding()
@@ -133,11 +171,84 @@ struct BustSheet : View {
         }
     }
 }
-
-
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct BetSheet : View {
+    let computerSum : Int
+    let playerSum : Int
+    @Binding var newBet: Int
+    
+    
+    var body: some View {
+        ZStack{
+            Color(red: 38/256, green: 108/256, blue: 59/256)
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                Text("Place your bet")
+                    .foregroundColor(.white)
+                    .font(.title)
+                Spacer()
+                
+                Text("Your current bet: \(newBet)$")
+                    .font(.largeTitle)
+                Spacer()
+                Button(action: {
+                    newBet += 5
+                }, label: {
+                    Text("Place 5$")
+                        .font(.largeTitle)
+                        .padding()
+                        .foregroundColor(Color.white)
+                })
+                .background(Color.red)
+                .cornerRadius(15.0)
+                
+                Button(action: {
+                    newBet += 10
+                    
+                }, label: {
+                    Text("Place 10$")
+                        .font(.largeTitle)
+                        .padding()
+                        .foregroundColor(Color.white)
+                })
+                .background(Color.red)
+                .cornerRadius(15.0)
+                .padding()
+                
+                Button(action: {
+                    newBet += 20
+                    
+                }, label: {
+                    Text("Place 20$")
+                        .font(.largeTitle)
+                        .padding()
+                        .foregroundColor(Color.white)
+                })
+                .background(Color.red)
+                .cornerRadius(15.0)
+                
+                Spacer()
+                Button(action: {
+                    newBet = 0
+                    
+                }, label: {
+                    Text("Reset")
+                        .font(.largeTitle)
+                        .padding()
+                        .foregroundColor(Color.white)
+                })
+                
+                
+            }
+        }
+        
+    }
+    
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
---
+++ DiceGame2.0/Model/BetData.swift
@@ -0,0 +1,12 @@
+//
+//  BetData.swift
+//  DiceGame2.0
+//
+//  Created by David Salmberg on 2023-04-06.
+//
+
+import Foundation
+
+class BetData: ObservableObject {
+    @Published var betAmount: Double = 0.0
+}
